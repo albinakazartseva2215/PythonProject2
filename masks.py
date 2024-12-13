547388c42fd924176b7b2f7fda400b16c3e6bcfe
albina_kazartseva <albinak74@gmail.com>
2024-12-13 18:19:29 +0500

    add _name_=_mane_

--- masks.py
+++ masks.py
@@ -14,5 +14,6 @@ def get_mask_account(account: int) -> str:
     return f"**{account_str[-4:]}"
 
 
-print(get_mask_card_number(7000792289606361))
-print(get_mask_account(73654108430135874305))
+if __name__ == "__main__":
+    print(get_mask_card_number(7000792289606361))
+    print(get_mask_account(73654108430135874305))
