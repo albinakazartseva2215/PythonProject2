322da99b736dd2a3ac82f07f2b0ed923b65f92a5
albina_kazartseva <albinak74@gmail.com>
2024-12-29 14:31:14 +0500

    "homework_10_2 ready"

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,9 +1,10 @@
 import pytest
-from src.masks import get_mask_card_number, get_mask_account
+
+from src.masks import get_mask_account, get_mask_card_number
 
 
 @pytest.fixture
-def card_number():
+def card_number() -> list[int]:
     return [7000792289606361, 7000792289606362]
 
 
@@ -17,9 +18,15 @@ def test_get_invalid_length_mask_card_number():
         get_mask_card_number(card_number=700079228960636)
 
 
+def test_get_without_mask_card_number():
+    with pytest.raises(TypeError):
+        get_mask_card_number(card_number=[])
+
+
 def test_get_invalid_type_mask_card_number(A000792289606361=None):
     with pytest.raises(TypeError):
         get_mask_card_number(card_number=A000792289606361)
+
 
 @pytest.fixture
 def account():
@@ -34,6 +41,7 @@ def test_get_mask_account(account, expected):
 def test_get_mask_account_invalid_length():
     with pytest.raises(ValueError):
         get_mask_account(account=736541084301358743056)
+
 
 def test_get_mask_account_invalid_type(A3654108430135874305=None):
     with pytest.raises(TypeError):
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,12 +1,20 @@
 import pytest
-from src.widget import mask_account_card, get_date
+
+from src.widget import get_date, mask_account_card
+
 
 @pytest.fixture
 def account_card():
     return ["Счет 73654108430135874305", "Visa Platinum 7000792289606361"]
 
-@pytest.mark.parametrize("account_card, expected", [("Счет 73654108430135874305", "Счет  **4305"),
-                                                    ("Visa Platinum 7000792289606361", "Visa Platinum  7000 79** **** 6361")])
+
+@pytest.mark.parametrize(
+    "account_card, expected",
+    [
+        ("Счет 73654108430135874305", "Счет  **4305"),
+        ("Visa Platinum 7000792289606361", "Visa Platinum  7000 79** **** 6361"),
+    ],
+)
 def test_get_mask_account_card(account_card, expected):
     assert mask_account_card(account_card) == expected
 
@@ -21,7 +29,18 @@ def date_str():
     return "2024-03-11T02:26:18.671407"
 
 
-@pytest.mark.parametrize("date_str, expected", [("2024-03-11T02:26:18.671407", "11.03.2024")])
-def test_get_date(date_str, expected):
-    assert get_date(date_str) == expected
+@pytest.mark.parametrize("date_str, format, expected", [("2024-03-11T02:26:18.671407", "%d.%m.%Y", "11.03.2024")])
+def test_get_date(date_str, format, expected):
+    assert get_date(date_str, format) == expected
 
+
+@pytest.mark.parametrize("format, expected", [("%d.%m.%Y", "11.03.2024")])
+def test_get_invalid_date(format, expected):
+    with pytest.raises(TypeError):
+        get_date(date_str, "%d-%m-%Y")
+
+
+@pytest.mark.parametrize("date_str, expected", [("2024-03-11T02:26:18.671407", "11.03.2024")])
+def test_get_without_date(date_str, expected):
+    with pytest.raises(TypeError):
+        get_date()
